101626a1f1d1698cffbef3f3a7b3d71184db9c15
Natan Ferreira <natan.ferreira21@hotmail.com>
2022-11-13 20:11:22 -0300

    Desenvolvendo primeiro projeto de banco

---
+++ Banco/src/Entities/BusinessAccount.java
@@ -0,0 +1,31 @@
+package Entities;
+
+public class BusinessAccount extends Account {
+	
+
+	private Double loanLimite;
+	
+	public BusinessAccount() {
+		super();
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimite) {
+		super(number, holder, balance);
+		this.loanLimite = loanLimite;
+	}
+
+	public Double getLoanLimite() {
+		return loanLimite;
+	}
+
+	public void setLoanLimite(Double loanLimite) {
+		this.loanLimite = loanLimite;
+	}
+	
+	public void loan(Double amount) {
+		if (amount <= loanLimite) {
+			balance += amount - 10.0;
+		}
+		
+	}
+}
